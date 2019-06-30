91bb48e5266ea873873cc569cf177f4bca4e303e
Gorbushkaa <qqqombo@gmail.com>
2019-06-30 18:11:07 +0300

    33

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask, abort, request, jsonify
+from flask import Flask, abort, request, jsonify, Response
 from pymongo import MongoClient
 import datetime
 from bson.objectid import ObjectId
-
+from functools import wraps
 
 client = MongoClient('localhost', 27017)
 db = client.restapi
 app = Flask(__name__)
+
+def check_auth(username, password):
+    a = db.users.find_one({'username': username,
+                      'password': password})
+    ID = str(a['_id'])
+    if a is not None:
+        return True and ID
+    if a is None:
+        return False
+
+
+def authenticate():
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 
 @app.route('/api/get_posts/', methods=['GET'])
 def check_posts():
@@ -15,6 +42,7 @@ def check_posts():
 
 
 @app.route('/api/check_in/', methods=['POST'])
+
 def create_user():
     content = request.json
     if len(content["email"]) <= 0 or len(content["username"]) <= 0 or len(content["password"]) <= 0:
@@ -36,6 +64,7 @@ def create_user():
             return 'Такой Email уже существует'
 
 @app.route('/api/new_post', methods=['POST'])
+@requires_auth
 def create_article():
     content = request.json
     if len(content["author_id"]) <= 0 or len(content["title"]) <= 0 or len(content["content"]) <= 0:
@@ -56,6 +85,7 @@ def create_article():
 
 
 @app.route('/api/new_comment', methods=['POST'])
+@requires_auth
 def new_comment():
     content = request.json
     if len(content["author_id"]) <= 0 or len(content["post_id"]) <= 0 or len(content["title"]) <= 0 or len(content["content"]) <= 0:
@@ -77,6 +107,48 @@ def new_comment():
 
         else:
             return 'Такого поста не существует'
+
+
+@app.route('/api/change/', methods=['PUT'])
+@requires_auth
+def update_post():
+
+    content = request.json
+    a = db.posts.find_one({'_id': ObjectId(content['post_id'])})
+    if a is not None and content['author_id'] == a['author_id']:
+        if len(content['title']) <= 0:
+            content['title'] = a['title']
+        elif len(content['content']) <= 0:
+            content['content'] = a['content']
+        old = {'_id': ObjectId(content['post_id']),
+                'content': a['content'],
+               'title': a['title']}
+        new = {"$set":{'content': content['content'],
+               'title': content['title']}}
+        db.posts.update_one(old, new)
+        return "Статья изменена"
+    else:
+        return 'Это не ваша статья'
+
+
+@app.route('/api/delete/', methods=['DELETE'])
+@requires_auth
+def delete():
+    content = request.json
+    if content['choose'] == 'post':
+        a = db.posts.find_one({'_id': ObjectId(content['post_id'])})
+        if a is not None:
+            db.posts.remove({'_id': ObjectId(content['post_id'])})
+            return 'Пост удален'
+        else:
+            return 'Пост не найден'
+    elif content['choose'] == 'comment':
+        a = db.comments.find_one({'_id': ObjectId(content['post_id'])})
+        if a is not None:
+            db.comments.remove({'_id': ObjectId(content['post_id'])})
+            return 'Коммент удален'
+        else:
+            return 'Коммент не найден'
 
 
 def encode(all):
--- aqq.py
+++ aqq.py
@@ -1,13 +1,24 @@
 import requests
+from requests.auth import HTTPBasicAuth
 '''url = 'http://127.0.0.1:5000/api/new_comment'
 send = {'post_id': '5d1727bf1bc709519742b68c',
         'author_id': '5d166e719b1b7d1188d608af',
         'title': 'TITLE',
         'content': 'COMMENT',
         }'''
+"""url = 'http://127.0.0.1:5000/api/change_post/'
+send = {'post_id': '5d18815010398fc4540b90db',
+        'author_id': '5d166e719b1b7d1188d608af',
+        'title': 'a32332af',
+        'content': '333',
+        }"""
 
 
-#url = 'http://127.0.0.1:5000/api/get_posts/'
+'''url = 'http://127.0.0.1:5000/api/delete/'
+send = {'post_id': '5d1882e133e705f0605538aa',
+        'choose': 'comment'}'''
+
+url = 'http://127.0.0.1:5000/api/get_posts/'
 
 '''url = 'http://127.0.0.1:5000/api/new_post'
 send = {"author_id": "5d166e719b1b7d1188d608af",
@@ -19,6 +30,6 @@ send = {"email": "g111212gg@gmail.com",
         "username": "asd33122ada",
         "password": "Seasd"}'''
 
-res = requests.post(url, json=send)
+res = requests.get(url, auth=('Sema', 'password1'))
 
 print(res.text)
